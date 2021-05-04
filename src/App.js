4c6b15b3b02aa7569cbe36c01137fd0524ab8ddf
Bouclair Israel <bouclair.israel@gmail.com>
2021-05-04 18:27:24 +0300

    added slider functions

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Header from './components/header/Header';
+import Main from './components/main/Main';
+import './App.scss';
 
 const App = () => {
   return (
     <Provider store={store}>
       <Header />
+      <div className="app">
+        <Main />
+      </div>
     </Provider>
   );
 };
---
+++ src/components/content/MainContent.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './MainContent.scss';
+import Slideshow from '../slide-show/Slideshow';
+
+const MainContent = () => {
+  const images = [
+    { url: 'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80' },
+    { url: 'https://images.unsplash.com/photo-1535016120720-40c646be5580?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80' },
+    { url: 'https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2125&q=80' }
+  ];
+  return (
+    <div className="main-content">
+      <Slideshow images={images} auto={true} showArrows={true}/>
+      <div className="grid-movie-title">
+        <div className="movieType">Now Playing</div>
+        <div className="paginate">Paginate</div>
+      </div>
+    </div>
+  );
+};
+
+export default MainContent;
